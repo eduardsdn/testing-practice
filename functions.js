45ccdd186050e3e69753ba3479d3e1cc9ee0cb03
eduardsdn <eduard.sdn@gmail.com>
2023-07-29 16:22:24 +0200

    Finish unit tests

--- functions.js
+++ functions.js
@@ -3,11 +3,11 @@ function capitalize(string) {
 }
 
 function reverseString(str) {
-    let newString = "";
+    let newString = ""
     for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
+        newString += str[i]
     }
-    return newString;
+    return newString
 }
 
 const calculator = {
@@ -24,40 +24,75 @@ const calculator = {
         return a*b
     },
 }
-// str, key
-function ceasarCipher() {
+
+function caesarCipher(str, key) {
     let encryptedStr = ""
-    str = "Humanoids are taking over the Earth! But, AhaHAhH"
-    key = 25
+
     for(let i = 0; i < str.length; i++) {
         if(str[i] === "," || str[i] === "." || str[i] === "!" || str[i] === "?" || str[i] === "-" || str[i] === " "){
             encryptedStr += str[i]
         }
         else if(str[i].toUpperCase() === str[i]){
-            encryptedStr += ceasarCipherEncryptCharacter(str[i], key, true)
+            encryptedStr += caesarCipherEncryptCharacter(str[i], key, true)
         }
-        else(encryptedStr += ceasarCipherEncryptCharacter(str[i], key, false))
+        else(encryptedStr += caesarCipherEncryptCharacter(str[i], key, false))
     }
-    console.log(encryptedStr)
+
+    return encryptedStr
 }
 
-function ceasarCipherEncryptCharacter(character, key, isUpperCase){
+function caesarCipherEncryptCharacter(character, key, isUpperCase){
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     const alphabetUpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    if(isUpperCase && (alphabetUpperCase.indexOf(character) + key + 1) > alphabetUpperCase.length) {
+        let wrappedIndex = (alphabetUpperCase.length - (alphabetUpperCase.indexOf(character) + key))*-1
+        let encryptedCharacter = alphabetUpperCase[wrappedIndex]
+        return encryptedCharacter
+    }
     if(isUpperCase) {
         let encryptedCharacter = alphabetUpperCase[alphabetUpperCase.indexOf(character) + key]
         return encryptedCharacter
     }
+    if((alphabet.indexOf(character) + key + 1) > alphabet.length) {
+        let wrappedIndex = (alphabet.length - (alphabet.indexOf(character) + key))*-1
+        let encryptedCharacter = alphabet[wrappedIndex]
+        return encryptedCharacter
+    } 
     else{
         let encryptedCharacter = alphabet[alphabet.indexOf(character) + key]
         return encryptedCharacter
     } 
-    
 }
 
-ceasarCipher()
+function analyzeArray(arr) {
+    let analysis = {
+        average: undefined,
+        min: undefined,
+        max: undefined,
+        length: undefined
+    }
 
-module.exports = {capitalize,  reverseString, calculator}
+    function calculateAverage(arr){
+        const arrLength = arr.length
+        let sum = 0 
+        arr.forEach((element) => {
+            sum += element
+        })
+        let average = sum/arr.length
+        return average
+    }
+
+    analysis.average = calculateAverage(arr)
+    analysis.min = Math.min(...arr)
+    analysis.max = Math.max(...arr)
+    analysis.length = arr.length
+
+    
+    return analysis
+}
+
+module.exports = {capitalize, reverseString, calculator, caesarCipher, analyzeArray}
 
 
 
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const {capitalize, reverseString, calculator} = require('./functions.js')
+const {capitalize, reverseString, calculator, caesarCipher, analyzeArray} = require('./functions.js')
 
 it('capitalize works', () => {
     string = "apple"
@@ -28,8 +28,6 @@ it('calculator mult works', () => {
     expect(calculator.multiply(a, b)).toEqual(20)
 })
 
-
-
 it('calculator division works', () => {
     a = 112
     b = 4
@@ -37,8 +35,48 @@ it('calculator division works', () => {
 })
 
 
-// it('', () => {
-    
-// })
+// caesarCipher
+it('Caesar cipher encrypts basic strings', () => {
+    str="hello"
+    key=1
+    expect(caesarCipher(str, key)).toMatch("ifmmp")
+})
+
+it('Caesar cipher encrypts basic strings with capital letters and punctuation', () => {
+    str="Unfortunately, No"
+    key=1
+    expect(caesarCipher(str, key)).toMatch("Vogpsuvobufmz, Op")
+})
+
+it('Caesar cipher encrypts basic strings with capital letters and punctuation, with larger keys', () => {
+    str="Unfortunately, No! I do not have the right. Do you?"
+    key=6
+    expect(caesarCipher(str, key)).toMatch("Atluxzatgzkre, Tu! O ju tuz ngbk znk xomnz. Ju eua?")
+})
+
+it('Caesar cipher encrypts strings with punctuation chars with the largest key', () => {
+    str="My name is Elena! Unfortunately, I do not live in Yerevan!"
+    key=25
+    expect(caesarCipher(str, key)).toMatch("Lx mzld hr Dkdmz! Tmenqstmzsdkx, H cn mns khud hm Xdqduzm!")
+})
+
+// analyzeArray 
+it('Analyze array returns correct average', () => {
+    arr = [1,8,3,4,2,6]
+    expect(analyzeArray(arr).average).toEqual(4)
+})
+it('Analyze array returns correct min', () => {
+    arr = [1,8,3,4,2,6]
+    expect(analyzeArray(arr).min).toEqual(1)
+})
+it('Analyze array returns correct max', () => {
+    arr = [1,8,3,4,2,6]
+    expect(analyzeArray(arr).max).toEqual(8)
+})
+it('Analyze array returns correct length', () => {
+    arr = [1,8,3,4,2,6]
+    expect(analyzeArray(arr).length).toEqual(6)
+})
+
 
 
